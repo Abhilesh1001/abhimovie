0d2914e583a5780ac959ff75b1aa5bb600d4fec6
Abhilesh1001 <104245983+Abhilesh1001@users.noreply.github.com>
2023-11-07 21:55:29 +0530

    homecsschange

--- src/component/home/moviedisplay/Moviedis.jsx
+++ src/component/home/moviedisplay/Moviedis.jsx
@@ -41,32 +41,33 @@ const Moviedis = () => {
 
     return (
         <div>
-            <button className='ml-20  hover:bg-sky-100 bg-blue-300 w-40 h-10 rounded'>MOVIES </button>
+            <div className='ml-[30px] mr-[30px]'>
+            <button className='  hover:bg-sky-100 bg-blue-300 w-40 h-10 rounded'>MOVIES </button>
             <button className='mx-2  hover:bg-sky-100 hover:rounded p-2'>TRENDING</button>
             <button className='mx-2  hover:bg-sky-100 rounded p-2'>MOSTVIEWED</button>
             <button className='mx-2 hover:bg-sky-100 p-2 rounded'>Top IMDB</button>
-            <div className='ml-20'>
-                <button className='mx-2 my-2 p-2 hover:bg-sky-100 rounded'>TVSERIES</button>
+            <button className='mx-2 my-2 p-2 hover:bg-sky-100 rounded'>TVSERIES</button>
             </div>
-            <div className='flex flex-wrap items-center justify-center cursor-pointer'>
+            <div className='flex flex-wrap items-center pl-[4px] pr-[4px] justify-center cursor-pointer'>
 
                 {
                     !loading && data?.results?.map((item, index) => {
                         const ImageUrl = url.backdrop + item.poster_path                       
                         return <div key={index} className='moviCart m-2 relative' onClick={(e)=>handleClick(item.id,item.media_type)} id={index} onMouseEnter={(event) => 
                             handleMouseEnter(event,index)} onMouseLeave={() => setIsHovered(null)}>                           
-                            <img src={`${ImageUrl}`} className='h-full w-full absolute rounded-md hover:filter hover:contrast-75'  alt="" />
-                            <div className='flex mx-2 flex-wrap relative' style={{top:'200px'}}>
-
+                            <img src={`${ImageUrl}`} className='h-full w-full absolute rounded-md  hover:filter hover:contrast-75'  alt="" />
+                            <div className='mx-2 flex-wrap relative genresetting'>
                             {/* adding genere here ...... */}
                             {item.genre_ids.slice(0,2).map((g,index1)=>{
                                 // console.log('genres',genres[g].name)
-                                return <div key={index1} style={{position:'relative',display:'flex'}} className='mx-1'>
+                                return <div key={index1} style={{position:'relative',display:'flex'}} className='mx-1 my-1'>
                                     <div className=' rounded  bgcolorgrad' style={{fontSize:'14px'}}>{genres[g]?.name}</div>
                                     </div>
                             })}
                             {/* end here...  */}
                             </div>
+
+
                             {/* hover effect starts here  */}
                             <div style={{position:'relative', top:`${divheight<400?"300px":'0px'}`,right:`${newdimention<300 ? "200px":"0px"}` }} >
                             {isHovered === index && <Moviedesc title={item.media_type==="movie"?item.title:item.name}  date={item.release_date} desc={item.overview
